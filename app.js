fb42a93fbdea7d41b7faf931c42a37918ce303d8
SaffetAdak <saffetadak_@hotmail.com>
2023-07-15 19:47:27 +0300

    First Release

---
+++ app.js
@@ -0,0 +1,39 @@
+const express = require("express");
+const https = require("https");
+const bodyParser = require("body-parser");
+
+const app = express();
+
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.get("/",function(req, res,) {
+    res.sendFile(__dirname + "/index.html");
+})
+
+app.post("/",function(req, res){
+    const city= req.body.cityName;
+    const url = "https://api.openweathermap.org/data/2.5/weather?q="+ city +"&appid=a450a76c4fd73d7a0ee7365d120cc251&units=metric";
+    https.get(url,function(response) {
+        console.log(response.statusCode);
+
+        response.on("data",function(data) {
+            const weatherData = JSON.parse(data);
+            const weatherInfo = weatherData.weather[0].description;
+            const temp = weatherData.main.temp;
+            const icon = weatherData.weather[0].icon;
+            const iconUrl = "https://openweathermap.org/img/wn/" + icon + "@2x.png"
+            res.write("<p>Weather is currently " + weatherInfo + "</p>");
+            res.write("<h1> The temperature in "+ city+" is " + temp + " degrees Celsius. </h1>")
+            res.write("<img src=" +iconUrl + ">")
+            res.send();
+        })
+    })
+
+})
+
+
+
+
+app.listen(3000, function(){
+    console.log("Server running on port 3000");
+})
